2775eb1628bb042842107e175cf84039b0c4c12e
ИС201-1 <IS201-1@tisbi.ru>
2023-12-05 16:24:06 +0300

    dsadsdsad

---
+++ NaitiKino/NaitiKino/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NaitiKino
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var user = App.DB.User.FirstOrDefault(x => x.Login == LoginTB.Text);
+            if (user == null && user.Password != PasswordTB.Text)
+            {
+                MessageBox.Show("Неверный логин или пароль");
+                return;
+            }
+            this.NavigationService.Navigate(new UserPage());
+        }
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegisterPage());
+        }
+    }
+}
